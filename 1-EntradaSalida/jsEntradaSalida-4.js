ba182916d85789bf4029c40c6f3c799d148fd985
alumno <alumno>
2019-02-06 19:11:55 -0300

    ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,10 @@
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
+{ var nombre;
+
+	nombre=prompt("por favor ingrese un dato");
+	elNombre.value=nombre;
 
 }
 
