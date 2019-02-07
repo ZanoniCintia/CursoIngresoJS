c5d89d5b4dd8990f93ac44f6be2ed75e8f49770f
alumno <alumno>
2019-02-06 21:24:28 -0300

    ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,74 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=numeroUno.value;
+    segundoNumero=numeroDos.value;
+    
+    primerNumero=parseInt(primerNumero);
+    segundoNumero=parseInt(segundoNumero);
+    
+    resultado=primerNumero+segundoNumero;
+
+    alert("resultado es:" +resultado) ;
+
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=numeroUno.value;
+    segundoNumero=numeroDos.value;
+    
+    primerNumero=(primerNumero);
+    segundoNumero=(segundoNumero);
+    
+    resultado=primerNumero-segundoNumero;
+
+    alert("resultado es:"+resultado) ;
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=numeroUno.value;
+    segundoNumero=numeroDos.value;
+    
+    primerNumero=(primerNumero);
+    segundoNumero=(segundoNumero);
+    
+    resultado=primerNumero*segundoNumero;
+
+    alert("resultado es:" +resultado) ;
 }
+	
+
 
 function dividir()
 {
 	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=numeroUno.value;
+    segundoNumero=numeroDos.value;
+    
+    primerNumero=(primerNumero);
+    segundoNumero=(segundoNumero);
+    
+    resultado=primerNumero/segundoNumero;
+
+    alert("resultado es:" +resultado) ;
 }
+
 
