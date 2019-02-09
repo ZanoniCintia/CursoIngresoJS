5ef732d6cda8b59cbaed6b6d7592cc7427510bb2
zanonicintia <cintu94@hotmail.com>
2019-02-09 12:54:57 -0300

    "ejercicio 9"

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,31 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{ 
+ var ingresesueldo;
+	var porcentaje;
+	var aumento;
+
+	ingresesueldo=sueldo.value;
+	
+	ingresesueldo=parseInt(ingresesueldo);
+	
+
+	porcentaje=ingresesueldo * 0.10;
+
+	// pocerntaje= sueldo*1.20
+
+	aumento= ingresesueldo+porcentaje;
+
+document.getElementById('resultado').value=aumento;
+
+
+    
+
+
+
+
+
 
 	
 }
