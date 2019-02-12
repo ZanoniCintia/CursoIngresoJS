8824726472a6019474d7c3754219ed4397ec03ec
alumno <alumno>
2019-02-11 22:14:39 -0300

    ejercicios if

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,27 @@
 function mostrar()
-{
-//tomo la edad  
+	{
+	//tomo la edad  
+	var LaEdad;
 
+	LaEdad=edad.value;
+
+	LaEdad=parseInt(LaEdad);
+
+	if(LaEdad>17)
+	{alert("usted es mayor");}
+
+	else
+	if(LaEdad<13)
+		{alert("usted es un niño");}
+	
+	else
+		{alert("usted es adolescente");}
+
+//if(LaEdad>12 && LaEdad<18)//
+//alert("usted es adolescente");}//
+
+//if(LaEdad<13)//
+//{alert("usted es un niño");}//
 
 
 
