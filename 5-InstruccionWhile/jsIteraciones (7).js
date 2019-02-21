c6258acdf89d7abc81e6fa7cd9c3cf86c4a8f577
alumno <alumno>
2019-02-20 22:15:25 -0300

    clase 20/02/19

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,6 +5,15 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+	while(respuesta=="si")
+	{
+
+		numero=prompt("ingrese numero");
+		contador=contador+1;
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+		respuesta=prompt("ingrese si para continuar");
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,27 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta='si';
+
+
+	while(respuesta=="si")
+	{
+
+		numero=prompt("ingrese numero");
+		contador=contador+1;
+		numero=parseInt(numero);
+		if(numero<0)
+		{
+			negativo=numero*negativo;
+
+		}else
+		{
+			positivo=numero+positivo;
+		}
+		respuesta=prompt("ingrese si para continuar");
+	}
+
 
 
 document.getElementById('suma').value=positivo;
