4ebf9c35b1d35670e37c5d7dc6db4ca49d846f2d
zanonicintia <cintu94@otmail.com>
2019-02-23 13:07:20 -0300

    "23/02"

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,15 +3,42 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var numero;
+	var maximo;
+	var numero;
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	
+		numero=pompt("ingrese numero");
+		if(contador==0)
+		{
+			maximo=numero;
+			minimo=numero;
+		}
+		else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+		}
+		if(numero>maximo)
+		{
+			maximo=numero;
+		}
+		if(numero<minimo)
+		{
+			minimo=numero;
+		}
+		contador=contador+1;
 	}
 
+
+
+respuesta=prompt("desea ingresar otro numero?");
+minimo.value=minimo;
+maximo.value=maximo;
 
 
 
