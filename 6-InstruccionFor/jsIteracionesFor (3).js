422f678b012e84e8878bbc4ef1581f6d13a92e1a
alumno <alumno>
2019-02-27 19:09:58 -0300

    for

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -3,6 +3,11 @@ function mostrar()
 
 var repetciones = prompt("ingrese el número de repeticiones");
 
+for(contador=1;contador<=repeticiones;)
+{
+	alert("hola UTNFRA");
+	contador=contador+1;
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,25 @@
 function mostrar()
 {
+	var numeroIngreso;
+	var incremento=1;
+	var divisor;
 
+	numeroIngreso=prompt("ingrese un numero");
+	numeroIngreso=parseInt(numeroIngreso);
+	
 
+	for(contador=0;incremento<=numeroIngreso;incremento++)
+	{
+		divisor=numeroIngreso%incremento;
+
+		if(divisor==0)
+		{
+			alert(incremento);
+			contador=contador+1;
+		}
+	}
+
+	alert("la cantidad de numero divisores :"+contador);
 
 
 }//FIN DE LA FUNCIÓN
